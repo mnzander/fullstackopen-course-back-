a29c4da0937c2fc871a16b8e9cc829002701152d
mnzander <ander.munoz.rivas@zabalburu.org>
2024-11-06 12:41:21 +0100

    features: Part 3d exercises

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginTypescript from '@typescript-eslint/eslint-plugin';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['node_modules/', 'dist/', '*.test.js'], // Replace .eslintignore with this property
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      react: eslintPluginReact,
+      '@typescript-eslint': eslintPluginTypescript
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn'
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
--- models/person.js
+++ models/person.js
@@ -14,18 +14,52 @@ mongoose.connect(url)
     console.log('error connecting to MongoDB:', error.message)
   });
 
-const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minLength: 4,
-    required: true
-  },
-  number: {
-    type: String,
-    minLength: 4,
-    required: true
+  const personSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      minLength: 3,
+      required: true
+    },
+    number: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function(value) {
+          return validateNumber(value);
+        },
+        message: '{VALUE} is not a valid number'
+      }
+    }
+  });
+
+  const validateNumber = (number) => {
+    const parts = number.split("-");
+
+    if (parts.length !== 2){
+      return false;
+    }
+
+    const part1 = parts[0];
+    const part2 = parts[1];
+
+    if(!(part1.length >= 2 && part1.length <= 3)) {
+      return false;
+    };
+
+    if (part2.length < 5) {
+      return false;
+    };
+
+    if (!/^\d+$/.test(part1) || !/^\d+$/.test(part2)) {
+      return false;
+    };
+
+    if (number.length < 8) {
+      return false;
+    };
+
+    return true;
   }
-});
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
